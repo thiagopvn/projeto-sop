59e988776a13c109e80e5806080371451aaa16ee
thiagopvn <thiago.luis2009@hotmail.com>
2025-06-29 23:06:08 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDWeThpt6h1Y3zxNenpGf7eTYPsUhVlgXI",
     authDomain: "projeto-sop.firebaseapp.com",
     projectId: "projeto-sop",
-    storageBucket: "projeto-sop.firebasestorage.app",
+    storageBucket: "projeto-sop.appspot.com",
     messagingSenderId: "509243342498",
     appId: "1:509243342498:web:29e941f3e16e7eea7721fb",
     measurementId: "G-H0D0G5SD3V"
